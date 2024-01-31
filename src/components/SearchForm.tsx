c52cf3a526459aab0b90c8e684a143ed5de5c110
Victor Reis <victorsireis@gmail.com>
2024-01-31 19:07:18 -0300

    style: ui improvement

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -7,6 +7,7 @@ const SearchForm = () => {
   const [query, setSearchText] = useState("");
   const { searchResults, loading, error, search } = useSearch();
 
+  console.log(searchResults);
   const handleSearch = async (e) => {
     e.preventDefault();
     search(query);
@@ -15,7 +16,7 @@ const SearchForm = () => {
   return (
     <div className="flex flex-col bg-background text-foreground items-center justify-center h-screen">
       {" "}
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center space-x-4 mb-12">
         <Input
           type="text"
           placeholder="Search..."
@@ -27,11 +28,17 @@ const SearchForm = () => {
           {loading ? "Searching..." : "Search"}
         </Button>
       </div>
-      <div className="mt-4">
+      <div className="grid gap-6 max-w-4xl">
         {searchResults.map((result) => {
           return (
-            <div key={result.id}>
-              <a href={result.url}>{result.name}</a>
+            <div key={result.id} className="px-14">
+              <a
+                href={result.url}
+                className="text-blue-700 hover:underline text-xl font-semibold"
+              >
+                {result.name}
+              </a>
+              <p>{result.snippet}</p>
             </div>
           );
         })}
